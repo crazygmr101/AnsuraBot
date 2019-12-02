d1c4000f1dc7e83fb862af8f7fbf549d1a23b7a4
Daniel Nash <dannash987@gmail.com>
2019-12-02 10:49:31 -0600

    add %xc alias

--- core/crosschat.py
+++ core/crosschat.py
@@ -6,12 +6,9 @@ class Crosschat:
     def __init__(self, bot: commands.Bot):
         self.channels = {}
         ch: str = open("Assets/crosschat.txt",'r').read()
-        print(ch)
         chl = ch.split("\n")
-        print(chl)
         chl = [x.lstrip().rstrip() for x in chl]
         for i in chl:
-            print(i)
             ar = i.split(":")
             self.channels[int(ar[0])] = int(ar[1])
         self.bot = bot
@@ -32,7 +29,8 @@ class Crosschat:
         e.add_field(name=user.name + "#" + str(user.discriminator)[0:2] + "xx", value=message.content)
         for k in self.channels.keys():
             if self.channels[k] == channel.id:
-                continue
+                pass
             c: discord.TextChannel = self.bot.get_channel(self.channels[k])
             if c is not None:
                 await c.send(embed=e)
+        await message.delete()
