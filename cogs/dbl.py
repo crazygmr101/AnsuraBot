9995aef25ff138d8d8d74a389159d4f37a785a77
Daniel Nash <dannash987@gmail.com>
2020-03-08 20:42:47 -0500

    Top.gg Server count

---
+++ cogs/dbl.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands, tasks
+import dbl
+import os
+
+class DBL(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+        self.token = os.getenv("DBL")
+        self.dblpy = dbl.DBLClient(self.bot, self.token, autopost=True)
+
+    @tasks.loop(seconds=600)
+    async def update_status(self):
+        await self.bot.change_presence(
+            status=discord.Status.online,
+            activity=discord.Activity(
+                name=str(len(self.bot.guilds)) + " servers | %help",
+                type=discord.ActivityType.watching
+            )
+        )
+
+def setup(bot):
+    bot.add_cog(DBL(bot))
