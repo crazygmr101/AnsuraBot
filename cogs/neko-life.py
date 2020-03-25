cf1a9e877586d4af3d9a9733a1949b6a3e3d646f
Daniel Nash <dannash987@gmail.com>
2020-03-25 01:26:32 -0500

    add poke, cuddle, and kiss commands

--- cogs/neko-life.py
+++ cogs/neko-life.py
@@ -1,6 +1,7 @@
 import discord
 import requests
 from discord.ext import commands
+import random
 
 class NekoLife(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -22,6 +23,31 @@ class NekoLife(commands.Cog):
         e.set_image(url=requests.get("https://nekos.life/api/v2/img/pat").json()["url"])
         await ctx.send(embed=e)
         await ctx.message.delete()
+
+    @commands.command()
+    async def poke(self, ctx: commands.Context, user: discord.Member):
+        e = discord.Embed()
+        e.title = f'{ctx.author.name} pokes {user.name}'
+        e.set_image(url=requests.get("https://nekos.life/api/v2/img/poke").json()["url"])
+        await ctx.send(embed=e)
+        await ctx.message.delete()
+
+    @commands.command()
+    async def cuddle(self, ctx: commands.Context, user: discord.Member):
+        e = discord.Embed()
+        e.title = f'{ctx.author.name} cuddles {user.name}'
+        e.set_image(url=requests.get("https://nekos.life/api/v2/img/cuddle").json()["url"])
+        await ctx.send(embed=e)
+        await ctx.message.delete()
+
+    @commands.command()
+    async def kiss(self, ctx: commands.Context, user: discord.Member):
+        e = discord.Embed()
+        e.title = f'{ctx.author.name} kisses {user.name}'
+        e.set_image(url=requests.get("https://nekos.life/api/v2/img/kiss").json()["url"])
+        await ctx.send(embed=e)
+        await ctx.message.delete()
+
 
 def setup(bot):
     bot.add_cog(NekoLife(bot))
