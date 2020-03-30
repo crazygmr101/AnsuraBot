ca4e25acf9ca69c0345f5fdc4a5aa05ecfc0228e
Daniel Nash <dannash987@gmail.com>
2020-03-30 03:11:47 -0500

    Remove maps cog

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ bot = commands.Bot(command_prefix=get_prefix)
 # bot.remove_command('help')
 xchat = Crosschat(bot)
 
-initial_extensions = ['cogs.gamertags', 'cogs.conversation', 'cogs.map', # 'cogs.help',
+initial_extensions = ['cogs.gamertags', 'cogs.conversation', 
                       'cogs.administration', 'cogs.misc', 'cogs.gaming',
                       'cogs.fun', 'cogs.owner', 'cogs.image',
                       'cogs.error-handler', 'cogs.streamer', 'cogs.confighandler',
