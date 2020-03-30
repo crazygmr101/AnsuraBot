73869f68e54b24757bdeb9f334f2a7fc2cc6a034
Daniel Nash <dannash987@gmail.com>
2020-03-30 02:23:00 -0500

    Rewrite gamertags cog help

--- cogs/gamertags.py
+++ cogs/gamertags.py
@@ -4,43 +4,60 @@ from typing import Union
 import pytz
 from discord.ext import commands
 import discord
-import core.help as HE
 import core.database as AD
 
 class Util(commands.Cog):
     def __init__(self, bot: discord.ext.commands.Bot):
         self.bot = bot
-        print("Util cog loaded")
+        print("Gamertags cog loaded")
         bot.db = AD.AnsuraDatabase()
         self.db = bot.db
 
     @commands.command()
     async def xbox(self, ctx: discord.ext.commands.Context, username):
+        """
+        Sets your xbox username
+        """
         self.db.set_gaming_record(ctx.author.id, "xboxlive", username)
         await ctx.send(ctx.author.mention + ": Set to " + username)
 
     @commands.command()
     async def mojang(self, ctx: discord.ext.commands.Context, username):
+        """
+        Sets your mojang username
+        """
         self.db.set_gaming_record(ctx.author.id, "mojang", username)
         await ctx.send(ctx.author.mention + ": Set to " + username)
 
     @commands.command(aliases=["yt"])
     async def youtube(self, ctx: discord.ext.commands.Context, username):
+        """
+        Sets your youtube username
+        """
         self.db.set_gaming_record(ctx.author.id, "youtube", username)
         await ctx.send(ctx.author.mention + ": Set to " + username)
 
     @commands.command()
     async def mixer(self, ctx: discord.ext.commands.Context, username):
+        """
+        Sets your mixer username
+        """
         self.db.set_gaming_record(ctx.author.id, "mixer", username)
         await ctx.send(ctx.author.mention + ": Set to " + username)
 
     @commands.command()
     async def twitch(self, ctx: discord.ext.commands.Context, username):
+        """
+        Sets your twitch username
+        """
         self.db.set_gaming_record(ctx.author.id, "twitch", username)
         await ctx.send(ctx.author.mention + ": Set to " + username)
 
     @commands.command(aliases=["gt"])
     async def gametags(self, ctx: discord.ext.commands.Context, user: Union[discord.Member, discord.User] = None):
+        """
+        Lists a user's gamertags
+        """
         if user is None:
             user = ctx.author
         rec = self.db.lookup_gaming_record(user.id)
@@ -62,6 +79,9 @@ class Util(commands.Cog):
 
     @commands.command(pass_context=True)
     async def who(self, ctx: discord.ext.commands.Context, tag: str):
+        """
+        Finds users with a gamertag of <tag>
+        """
         db = self.db
         records = db.get_all()
         #id, mojang, xbox, youtube, twitch, mixer
@@ -86,15 +106,3 @@ class Util(commands.Cog):
 
 def setup(bot):
     bot.add_cog(Util(bot))
-    HE.HelpEntries.register("ping", "%ping", "Checks bot latency")
-    HE.HelpEntries.register("xbox", "%xbox username", "Sets your xbox username")
-    HE.HelpEntries.register("mojang", "%mojang username", "Sets your mojang username")
-    HE.HelpEntries.register("mixer", "%mixer username", "Sets your mixer username")
-    HE.HelpEntries.register("youtube", "%youtube username", "Sets your youtube username", "Alias: yt")
-    HE.HelpEntries.register("twitch", "%twitch username", "Sets your twitch username")
-    HE.HelpEntries.register("gametags", "%gametags @user", "Look up a user's gamertags", "Alias: gt")
-    HE.HelpEntries.register("who","%who tag","Find a user#1234 by gamertag")
-    HE.HelpEntries.register("role", "%role rolename", "Lists users in a role")
-    HE.HelpEntries.register("embed", "%embed title description channel color <id if editing>",
-                            "Sends or edits an embed", "`channel` defaults to current channel\n"
-                                                       "`color` defaults to author's role color")
