12c80e16eca837844fa31c3082fef4d7fdbdaa67
Daniel Nash <dannash987@gmail.com>
2020-04-03 23:50:18 -0500

    update guild commands

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,6 +1,7 @@
+import asyncio
 import re
 from typing import List, Union
-
+import cogs
 from discord.ext import commands
 import discord
 import random
@@ -104,42 +105,51 @@ class Owner(commands.Cog):
     @commands.command()
     @commands.is_owner()
     async def guilds(self, ctx: commands.Context):
-        """
-        Gets the list of guilds Ansura is in.
-        """
+        tts: cogs.tts.TTS = self.bot.get_cog("TTS")
         g: discord.Guild
-        i = 0
-        for g in self.bot.guilds:
-            await ctx.send(str(i) + ":" + g.name)
-            self.guilds.append(g)
-            i += 1
+        m: discord.Message = await ctx.send("Building list...")
+        s = """```"""
+        async with ctx.typing():
+            for g in self.bot.guilds:
+                s += f"{g.id} - {g.name}\n"
+        await m.edit(content=s + "```")
 
     @commands.command()
     @commands.is_owner()
-    async def ginfo(self, ctx: commands.Context, n: int):
-        """
-        Gets basic guild info of a guild from %guilds
-        - n: guilds list index
-        """
-        if n < 0 or n >= len(self.guilds):
-            await ctx.send("Invalid number")
-            return
-        g: discord.Guild = self.guilds[n]
-        e = discord.Embed()
-        e.title = g.name
-        e.set_image(url=g.icon_url)
-        e.description = g.description
-        e.add_field(name="Members", value=str(g.member_count))
-        e.add_field(name="Region", value=str(g.region))
-        e.add_field(name="ID", value=str(g.id))
+    async def ginfo(self, ctx: commands.Context, id: int):
+        async with ctx.typing():
+            if id not in [g.id for g in self.bot.guilds]:
+                await ctx.send("I'm not in a guild with that ID")
+                return
+            m: discord.User
+            g: discord.Guild = self.bot.get_guild(id)
+            users, bots = 0, 0
+            for m in g.members:
+                if m.bot: bots += 1
+                else: users += 1
+            e = discord.Embed()
+            e.title = g.name
+            e.set_thumbnail(url=g.icon_url)
+            e.description = g.description
+            e.add_field(name="Total Members", value=g.member_count)
+            e.add_field(name="Users/Bots", value=f"{users}/{bots}")
+            e.add_field(name="Region", value=str(g.region))
+            e.add_field(name="ID", value=str(g.id), inline=False)
+            e.add_field(name="Owner", value=f"<@{g.owner_id}> ({g.owner_id})", inline=False)
         await ctx.send(embed=e)
 
     @commands.command()
     @commands.is_owner()
     async def guild_leave(self, ctx: commands.Context, id: int):
         """Leaves a guild given by <id>"""
-        g: discord.Guild = await self.bot.fetch_guild(id)
+        g: discord.Guild = await self.bot.get_guild(id)
+        if g is None:
+            await ctx.send("I'm not in a guild with this ID")
+            return
+        s = g.name
         await g.leave()
+        await ctx.send(f"Left {s}")
+
 
 
 
