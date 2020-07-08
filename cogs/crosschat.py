8d6a97fc20eee641793da3edec10b397d026a5f4
Daniel Nash <dannash987@gmail.com>
2020-07-08 14:17:53 -0500

    add ability to ban people from xchat and add crosschat command

--- cogs/crosschat.py
+++ cogs/crosschat.py
@@ -1,47 +1,132 @@
+from typing import Dict, List, Optional, Union
+
 import discord
 import discord.errors
 from discord import Guild, TextChannel
 from discord.ext import commands
-
+from ruamel.yaml import YAML
+from disputils import BotEmbedPaginator
+from lib.utils import pages
 
 class Crosschat(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.colors = {}
-        self.channels = {}
         self.bot = bot
-        self._cd = commands.CooldownMapping.from_cooldown(2, 60, commands.BucketType.user)
+        self._cd = commands.CooldownMapping.from_cooldown(3, 50, commands.BucketType.user)
+        self.channels: Optional[Dict[str, int]] = None
+        self.banned: Optional[List[int]] = None
+
+    @commands.command()
+    @commands.is_owner()
+    async def xcreload(self):
+        self._reload()
+
+    def _reload(self):
+        with open("xchat.yaml") as fp:
+            config = YAML().load(fp)
+        self.channels = config["channels"]
+        self.banned = config["banned"]
+        for i in self.channels:
+            color = int(i) // 64 % (14 ** 3) + 0x222
+            rd = color >> 8
+            gr = (color & 0x0f0) >> 4
+            bl = (color & 0xf)
+            self.colors[int(i)] = discord.Colour.from_rgb(rd * 0x11, gr * 0x11, bl * 0x11)
+
+    @commands.command()
+    @commands.is_owner()
+    async def xcbans(self, ctx: commands.Context):
+        await BotEmbedPaginator(ctx,
+                                pages(
+                                    [f"{self.bot.get_user(x)} - {x}" for x in self.banned],
+                                    10, "Crosschat bans"
+                                )).run()
+
+    @commands.command()
+    @commands.is_owner()
+    async def xcservers(self, ctx: commands.Context):
+        await BotEmbedPaginator(ctx,
+                                pages(
+                                    [f"{self.bot.get_guild(int(x))} ({x}) - "
+                                     f"{self.bot.get_channel(c)} ({c})" for x, c in self.channels.items()],
+                                    10, "Crosschat servers"
+                                )).run()
+
+    @commands.command()
+    @commands.is_owner()
+    async def crosschat(self, ctx: commands.Context, arg: Union[discord.TextChannel, str] = None):
+        if not arg:
+            if ctx.guild.id in self.channels.keys():
+                await ctx.send(f"Crosschat is set to <#{self.channels[ctx.guild.id]}>. Do "
+                               f"`%crosschat #channel` to change this or `%crosschat clear` to "
+                               f"turn off crosschat")
+            else:
+                await ctx.send(f"Crosschat is not enabled on this server. Do "
+                               f"`%crosschat #channel` to change this.")
+            return
+        if isinstance(arg, discord.TextChannel):
+            self.channels[ctx.guild.id] = arg.id
+            await ctx.send(f"Crosschat is set to <#{self.channels[ctx.guild.id]}>. Do "
+                           f"`%crosschat #channel` to change this or `%crosschat clear` to "
+                           f"turn off crosschat")
+        elif arg == "clear":
+            del self.channels[ctx.guild.id]
+            await ctx.send(f"Crosschat channel cleared. Do `%crosschat #channel` to change this.")
+        else:
+            return
+        with open("xchat.yaml", "w") as fp:
+            YAML().dump({"banned": self.banned, "channels": self.channels}, fp)
+
+
+    @commands.command()
+    @commands.is_owner()
+    async def xcban(self, ctx: commands.Context, member: Union[discord.Member, int]):
+        if isinstance(member, discord.Member):
+            member = member.id
+        if member not in self.banned:
+            self.banned.append(member)
+            with open("xchat.yaml", "w") as fp:
+                YAML().dump({"banned": self.banned, "channels": self.channels}, fp)
+            await ctx.send(f"{self.bot.get_user(member)} ({member}) xchat banned")
+        else:
+            await ctx.send(f"{self.bot.get_user(member)} ({member}) already xchat banned")
+
+    @commands.command()
+    @commands.is_owner()
+    async def xcunban(self, ctx: commands.Context, member: Union[discord.Member, int]):
+        if isinstance(member, discord.Member):
+            member = member.id
+        if member in self.banned:
+            self.banned.remove(member)
+            with open("xchat.yaml", "w") as fp:
+                YAML().dump({"banned": self.banned, "channels": self.channels}, fp)
+            await ctx.send(f"{self.bot.get_user(member)} ({member}) xchat unbanned")
+        else:
+            await ctx.send(f"{self.bot.get_user(member)} ({member}) already not banned")
 
     async def init_channels(self):
         print("[XCHAT] Looking for channels")
-        g: Guild
-        c: discord.TextChannel
-        for g in self.bot.guilds:
-            for c in g.channels:
-                if type(c) is not TextChannel:
-                    continue
-                if c.topic is None:
-                    continue
-                if "ansura crosschat" in c.topic or c.topic == "ansura crosschat":
-                    print(" FOUND " + c.name + " (" + str(c.id) + ") in " + g.name + " (" + str(g.id) + ")")
-                    color = int(g.id / 64) % (14 ** 3) + 0x222
-                    rd = color >> 8
-                    gr = (color & 0x0f0) >> 4
-                    bl = (color & 0xf)
-                    self.channels[g.id] = int(c.id)
-                    self.colors[g.id] = discord.Colour.from_rgb(rd * 0x11, gr * 0x11, bl * 0x11)
-                    break
+        self._reload()
+        print(f" - Found {len(self.channels)} channels")
+        print(f" - Found {len(self.banned)} banned members")
         print("[XCHAT] Channel search done")
 
     async def xchat(self, message: discord.Message):
+        channel: discord.TextChannel = message.channel
+        if channel.id not in self.channels.values():
+            return
+        if message.author.id in self.banned:
+            try:
+                await message.delete()
+            except discord.errors.Forbidden:
+                pass
+            return
         if self._cd.get_bucket(message).update_rate_limit():
             try:
                 await message.delete()
             except discord.errors.Forbidden:
                 pass
             await message.channel.send(f"{message.author.mention}, you're sending messages too fast!", delete_after=30)
-            return
-        channel: discord.TextChannel = message.channel
-        if channel.id not in self.channels.values():
             return
 
         guild: discord.Guild = channel.guild
--- lib/utils.py
+++ lib/utils.py
@@ -6,3 +6,92 @@ def descend(obj: Dict, keys: List[str]):
     for k in keys:
         temp = temp[k]
     return temp
+
+import asyncio
+from typing import Any, List
+
+import discord
+from discord.ext import commands
+
+NBSP = "͔"
+
+
+def letter_emoji(a: str):
+    if a.isascii() and a.isalpha() and len(a) == 1:
+        a = a.upper()
+    else:
+        return None
+    return chr(ord(a[0]) + 0x1f1a5)
+
+
+def quote(st: str):
+    return "\n".join(f"> {n}" for n in st.split("\n"))
+
+
+def trim(st: str, length: int = 300) -> str:
+    """
+    Trims a string to be less than `length` characters
+    :param st: the string to trim
+    :param length: the length to trim to
+    :return: the trimmed string
+    """
+    return st if len(st) < length else st[:length]
+
+
+async def trash_reaction(msg: discord.Message, bot: commands.Bot, ctx: commands.Context):
+    """
+    Puts a "trash can" on a message, so the calling user can delete it
+    :param msg: the :class:`discord.Message` the reaction gets added to
+    :param bot: the :class:`discord.ext.commands.Bot`
+    :param ctx: the :class:`discord.ext.commands.Context`
+    :return:
+    """
+
+    def check(_reaction: discord.Reaction, _user: discord.User):
+        return _user.id == ctx.author.id and _reaction.message.id == msg.id and str(_reaction) == "🗑️"
+
+    await msg.add_reaction("🗑️")
+    try:
+        _, _ = await bot.wait_for("reaction_add", timeout=60.0, check=check)
+    except asyncio.TimeoutError:
+        await msg.clear_reactions()
+    else:
+        await msg.delete()
+
+
+def group_list(lst: List[Any], n: int) -> List[List[Any]]:
+    """
+    Splits a list into sub-lists of n
+    :param lst: the list
+    :param n: the subgroup size
+    :return: The list of lists
+    """
+    return [lst[i * n:(i + 1) * n] for i in range((len(lst) + n - 1) // n)]
+
+
+def pages(lst: List[Any], n: int, title: str, *, fmt: str = "```%s```", sep: str = "\n") -> List[discord.Embed]:
+    # noinspection GrazieInspection
+    """
+        Paginates a list into embeds to use with :class:disputils.BotEmbedPaginator
+        :param lst: the list to paginate
+        :param n: the number of elements per page
+        :param title: the title of the embed
+        :param fmt: a % string used to format the resulting page
+        :param sep: the string to join the list elements with
+        :return: a list of embeds
+        """
+    l: List[List[str]] = group_list([str(i) for i in lst], n)
+    pgs = [sep.join(page) for page in l]
+    return [
+        discord.Embed(
+            title=f"{title} - {i + 1}/{len(pgs)}",
+            description=fmt % pg
+        ) for i, pg in enumerate(pgs)
+    ]
+
+
+def numbered(lst: List[Any]) -> List[str]:
+    """
+    Returns a numbered version of a list
+    """
+    return [f"{i} - {a}" for i, a in enumerate(lst)]
