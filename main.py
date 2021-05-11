c7185faae61e3e7ff3e165e103ea48035a78430e
crazygmr101 <dannash987@gmail.com>
2021-05-10 20:04:20 -0400

    update cog name

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ bot.initial_extensions = ['cogs.gamertags',
                           'cogs.voice',
                           'cogs.help',
                           'cogs.crosschat',
-                          "slash.minecraft_slash"]
+                          "slash.minecraft"]
 if __name__ == '__main__':
     for ext in bot.initial_extensions:
         print(f"[COGS] Loading {ext}")
